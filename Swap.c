5d513a866fd9579e9b9f3cd3f82e74edea15a692
AfsheenTabassum <71068046+AfsheenTabassum@users.noreply.github.com>
2020-09-11 12:17:49 +0530

    Create swap.c
    As Double datatype has been used you can give an integer as well as a decimal input.

---
+++ Swap.c
@@ -0,0 +1,26 @@
+//Program to perform Swapping of two numbers without using a temporary variable
+#include <stdio.h>
+int main()
+ {
+    double a, b;
+    printf("Enter the value of a:");
+    scanf("%lf", &a); //here %lf is the format specifier for double datatype
+    printf("Enter the value of b:");
+    scanf("%lf", &b);
+
+//Values of 'x' and 'y' are given by the user
+
+//Swapping process begins from here
+
+//initial values are the input values
+ 
+    a = a - b;   //a = ( initial_a - initial_b ) here 'a' value is changed i.e, changed_a
+    
+    b = a + b;   //b = ( changed_a + initial_b ) here 'b' value is changed i.e, changed_b = initial_a
+
+    a = b - a;   //a = ( changed_a - changed_b ) here 'a' value is changed i.e, changed_a = initial_b
+
+    printf("After swapping: a = %lf\n", a); //The swapped value of 'a' is given in the place of %lf , \n is used to continue from the new line
+    printf("After swapping: b = %lf", b);   //The swapped value of 'b' is given in the place of %lf 
+    return 0;  
+}
